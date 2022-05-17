6ca9d280747c96a07378d9d72227cee64ff40a70
Chang Woo Myung <47852311+changwmyung@users.noreply.github.com>
2022-05-17 12:29:14 +0900

    initial commit

--- io.py
+++ io.py
@@ -2,6 +2,15 @@ import numpy as np
 from ase.io.trajectory import Trajectory
 from ase.io import read,write
 from ase.build import sort
+from ase import Atoms
+from ase.constraints import FixBondLengths
+from ase.calculators.tip3p import TIP3P, rOH, angleHOH
+from ase.md import Langevin
+import ase.units as units
+from ase.io.trajectory import Trajectory
+import numpy as np
+from ase.io import write
+from ase.build import sort
 
 def return_last_frame(filename='md.traj',format='vasp'):
     '''
@@ -24,3 +33,41 @@ def return_min_structure(filename='structures.traj'):
     minarg=a[np.argmin(ene)]
 
     write('POSCAR_cand',minarg)
+
+
+def aqs_gen(pos_s=[[0, 0, 0]],mols='Cl',format='vasp')
+    '''
+    Generate aqueous solution input file
+    mols: solvent molecule
+    pos_s: position info of solvent molecule
+    '''
+
+    # Set up water box at 20 deg C density
+    x = angleHOH * np.pi / 180 / 2
+    pos = [[0, 0, 0],
+           [0, rOH * np.cos(x), rOH * np.sin(x)],
+           [0, rOH * np.cos(x), -rOH * np.sin(x)]]
+    atoms = Atoms('OH2', positions=pos)
+    
+    vol = ((18.01528 / 6.022140857e23) / (0.9982 / 1e24))**(1 / 3.)
+    atoms.set_cell((vol, vol, vol))
+    atoms.center()
+    
+    atoms = atoms.repeat((3, 3, 3))
+    atoms.set_pbc(True)
+    del atoms[:3]
+    
+    #solvent molecule
+    solvent = Atoms(mols, positions=pos_s)
+    vol = ((18.01528 / 6.022140857e23) / (0.9982 / 1e24))**(1 / 3.)
+    solvent.set_cell((vol, vol, vol))
+    solvent.center()
+    
+    atoms+=solvent
+    
+    atoms=sort(atoms)
+    
+    if format == 'vasp':
+        write('POSCAR',atoms)
+    else: 
+        write('init.xyz',atoms,format='extxyz')
